0b7ac7486ec5ed2d9ac02a45a48f66e71a82d67a
hctrdubs24 <OtracuentadeHector24@outlook.com>
2022-10-03 19:38:45 -0500

    test344

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,10 +9,10 @@ class MyDocument extends Document {
             name="description"
             content="Dev AT E-commerce website with Next.js"
           />
-          <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-          <script
+          <Script src="https://kit.fontawesome.com/a076d05399.js"></Script>
+          <Script
             src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}
-          ></script>
+          ></Script>
 
           {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
